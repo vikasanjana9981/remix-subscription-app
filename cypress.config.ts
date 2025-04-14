4fcf2e5f6b8a774dcc06515c1854552b84db8065
vikasanjana <va6903@gmail.com>
2025-04-14 19:44:33 +0530

    fix cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    // Just remove or comment this section to skip custom logic
+    // setupNodeEvents: (on, config) => {
+    //   // Custom plugin logic here...
+    // },
+  },
+});
