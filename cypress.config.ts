f86ab57f9017cc07662446d339946be4191576e4
vikasanjana <va6903@gmail.com>
2025-04-14 19:27:41 +0530

    Fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,18 +4,16 @@ export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
-      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+      const port: string = process.env.PORT || (isDev ? "3000" : "8811");
+
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
         baseUrl: `http://localhost:${port}`,
         screenshotOnRunFailure: !process.env.CI,
       };
 
-      // To use this:
-      // cy.task('log', whateverYouWantInTheTerminal)
       on("task", {
         log: (message) => {
           console.log(message);
-
           return null;
         },
       });
